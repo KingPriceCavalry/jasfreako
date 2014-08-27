0e0939dc31c081d8baf041f1b7642bfcc506a213
jasfreako <jas.rademeyer@kingprice.co.za>
2014-08-27 21:15:10 +0200

    Testing Git with Visual Studio

--- KPSchool_P1/KPSchool_P1/Program.cs
+++ KPSchool_P1/KPSchool_P1/Program.cs
@@ -11,6 +11,7 @@ namespace KPSchool_P1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I just used Git in Visual Studio.");
             Console.ReadLine();
         }
     }
